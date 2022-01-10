f98e43a71bc979e9e0a1b9c431667e06df8687a7
William Roebuck <16RoebuW@fulford.york.sch.uk>
2022-01-10 13:40:01 +0000

    Typed in some random scores for people, close #2

--- ListChallenge/Program.cs
+++ ListChallenge/Program.cs
@@ -11,6 +11,7 @@ namespace ListChallenge
         static void Main(string[] args)
         {
             List<string> names = new List<string> { "Will 1", "Will 2", "Ethan", "Adam", "Ben", "Tom", "Cormac", "Ashton", "Henry", "Mr. Dring"};
+            List<int> scores = new List<int> { 0, 20, 521, 41, 3, 12, 876, 86, 675, 6};
 
             Console.WriteLine(names[3]);
         }
